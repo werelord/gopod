a597a2ef534f8e01e89a047dddae6a3023699967
jb <1413880+werelord@users.noreply.github.com>
2022-07-19 08:20:08 -0500

    missed from staging last commit

--- xmlparse.go
+++ xmlparse.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 	"time"
 
@@ -18,18 +19,18 @@ import (
 
 //--------------------------------------------------------------------------
 type XChannelData struct {
-	title       string
-	pubdate     time.Time
-	link        string
-	image       XChannelImage
-	author      string
-	description string
+	Title       string
+	LastPubDate time.Time
+	Link        string
+	Image       XChannelImage
+	Author      string
+	Description string
 }
 
 type XChannelImage struct {
-	url   string
-	title string
-	link  string
+	Url   string
+	Title string
+	Link  string
 }
 
 type XItemData struct {
@@ -37,7 +38,7 @@ type XItemData struct {
 	pubdate     time.Time
 	guid        string
 	link        string
-	image       string
+	imageurl    string
 	description string
 	enclosure   XEnclosureData
 }
@@ -48,12 +49,18 @@ type XEnclosureData struct {
 	url     string
 }
 
+type feedProcess interface {
+	maxDuplicates() uint
+	exists(hash string) bool
+	checkTimestamp(timestamp time.Time) bool
+}
+
 //--------------------------------------------------------------------------
-func parseXml(xmldata []byte, maxDupes uint, hashExists func(string) bool) (feedData XChannelData, feedlist map[string]XItemData, err error) {
+func parseXml(xmldata []byte, fp feedProcess) (feedData XChannelData, newItems map[string]XItemData, err error) {
 	// fuck the ignore list
 	//ignoreList := []string{"atom:link", "lastBuildDate"}
 
-	feedlist = make(map[string]XItemData)
+	newItems = make(map[string]XItemData)
 
 	doc := etree.NewDocument()
 	if err = doc.ReadFromBytes(xmldata); err != nil {
@@ -63,7 +70,10 @@ func parseXml(xmldata []byte, maxDupes uint, hashExists func(string) bool) (feed
 
 	root := doc.SelectElement("rss").SelectElement("channel")
 	//log.Debug("root element: " + root.Tag)
-	var dupItemCount uint = 0
+	var (
+		dupItemCount uint = 0
+		maxDupes          = fp.maxDuplicates()
+	)
 
 	for _, elem := range root.ChildElements() {
 
@@ -71,31 +81,28 @@ func parseXml(xmldata []byte, maxDupes uint, hashExists func(string) bool) (feed
 		// todo: later use reflection
 		switch {
 		case strings.EqualFold(elem.FullTag(), "title"):
-			feedData.title = elem.Text()
+			feedData.Title = elem.Text()
 		case strings.EqualFold(elem.FullTag(), "pubdate"):
-			t, e := dateparse.ParseAny(elem.Text())
-			if e != nil {
-				log.Debug("Error parsing timestamp: " + elem.Text())
-			} else {
-				feedData.pubdate = t
-			}
+			// todo: shortcut, check pub date
+			// we're assuming this would be early in the process
+			feedData.LastPubDate = parseDateEntry(elem.Text())
 		case strings.EqualFold(elem.FullTag(), "link"):
-			feedData.link = elem.Text()
+			feedData.Link = elem.Text()
 		case strings.EqualFold(elem.FullTag(), "image"):
 			if urlnode := elem.SelectElement("url"); urlnode != nil {
-				feedData.image.url = elem.SelectElement("url").Text()
+				feedData.Image.Url = elem.SelectElement("url").Text()
 			}
 			if titlenode := elem.SelectElement("title"); titlenode != nil {
-				feedData.image.title = titlenode.Text()
+				feedData.Image.Title = titlenode.Text()
 			}
 			if linknode := elem.SelectElement("link"); linknode != nil {
-				feedData.image.link = linknode.Text()
+				feedData.Image.Link = linknode.Text()
 			}
 
 		case strings.EqualFold(elem.FullTag(), "itunes:author"):
-			feedData.author = elem.Text()
+			feedData.Author = elem.Text()
 		case strings.EqualFold(elem.FullTag(), "description"):
-			feedData.description = elem.Text()
+			feedData.Description = elem.Text()
 		case strings.EqualFold(elem.FullTag(), "item"):
 			// adding items to a slice, for future handling without iterating thru the childlist again
 
@@ -109,8 +116,8 @@ func parseXml(xmldata []byte, maxDupes uint, hashExists func(string) bool) (feed
 					continue
 				}
 
-				exists := hashExists(hash)
-				log.Debugf("hash: '%+v', exists: '%+v'", hash, exists)
+				exists := fp.exists(hash)
+				//log.Debugf("hash: '%+v', exists: '%+v'", hash, exists)
 
 				if exists {
 					// exists, increment the dup counter
@@ -119,7 +126,9 @@ func parseXml(xmldata []byte, maxDupes uint, hashExists func(string) bool) (feed
 				} else {
 					// todo: parse this shit
 					if item, e := parseItemEntry(elem); e == nil {
-						feedlist[hash] = item
+						newItems[hash] = item
+					} else {
+						log.Errorf("not adding item '", hash, "': ", e)
 					}
 				}
 			} else {
@@ -160,20 +169,66 @@ func calcHash(elem *etree.Element) (string, error) {
 	}
 
 	if guid == "" && urlstr == "" {
-		return "", errors.New("failed to hash item; guid and url not parsed!")
+		return "", errors.New("failed to hash item; guid and url not parsed")
 	}
 
 	// at least url or guid exists, hash the combination
 	sha := sha1.New()
 	sha.Write([]byte(guid + urlstr))
 	hash := base64.URLEncoding.EncodeToString(sha.Sum(nil))
-	log.Debugf("guid: '%+v', url: '%+v', hash: '%+v'", guid, urlstr, hash)
+	//log.Debugf("guid: '%+v', url: '%+v', hash: '%+v'", guid, urlstr, hash)
 
 	return hash, nil
 }
 
 //--------------------------------------------------------------------------
+func parseDateEntry(date string) time.Time {
+	t, e := dateparse.ParseAny(date)
+	if e != nil {
+		log.Debug("Error parsing timestamp: ", date)
+	}
+	return t
+
+}
+
+//--------------------------------------------------------------------------
 func parseItemEntry(elem *etree.Element) (item XItemData, err error) {
 
+	for _, child := range elem.ChildElements() {
+		switch {
+		case strings.EqualFold(child.FullTag(), "title"):
+			item.Title = child.Text()
+		case strings.EqualFold(child.FullTag(), "pubdate"):
+			item.pubdate = parseDateEntry(child.Text())
+		case strings.EqualFold(child.FullTag(), "guid"):
+			item.guid = child.Text()
+		case strings.EqualFold(child.FullTag(), "link"):
+			item.link = child.Text()
+		case strings.EqualFold(child.FullTag(), "itunes:image"):
+			if href := child.SelectAttr("href"); href != nil {
+				item.imageurl = href.Value
+			}
+		case strings.EqualFold(child.FullTag(), "description"):
+			item.description = child.Text()
+		case strings.EqualFold(child.FullTag(), "enclosure"):
+			if lenStr := child.SelectAttr("length"); lenStr != nil {
+				if l, e := strconv.Atoi(lenStr.Value); e == nil {
+					item.enclosure.length = uint(l) // shouldn't be any overflow, or negatives, hopefully
+				} else {
+					log.Error("error in parsing enclosure length:", e)
+				}
+			}
+			if typestr := child.SelectAttr("type"); typestr != nil {
+				item.enclosure.typeStr = typestr.Value
+			}
+			// url is required
+			if url := child.SelectAttr("url"); url != nil {
+				item.enclosure.url = url.Value
+			} else {
+				err = errors.New("missing url")
+			}
+		}
+	}
+	//log.Debugf("%+v", item)
 	return
 }
